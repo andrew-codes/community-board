540b5c070d731f1edd5855e9337bfa26fa11537b
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-06-22 07:40:03 -0400

    #27 Common utilities for creating stores and working with action creators

---
+++ src/lib/utils/redux.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import _ from 'lodash';
+import uniqueId from 'uniqueid';
+
+export const createActions = (actionObj) => {
+    const baseId = uniqueId();
+    return _.zipObject(_.map(actionObj, (actionCreator, key) => {
+        const actionId = `${baseId}-${key}`;
+        const method = (...args) => {
+            const result = actionCreator(...args);
+
+            if (result instanceof Promise) {
+                // Promise (async)
+                return {
+                    types: ['BEGIN', 'SUCCESS', 'FAILURE'].map( (state) => `${actionId}-${state}`),
+                    promise: result,
+                }
+            } else if (typeof result === 'function') {
+                // Function (async)
+                return (...args) => {
+                    return {
+                        type: actionId,
+                        ...(result(...args) || {})
+                };
+            }
+        } else {
+            // Object (sync)
+            return {
+                type: actionId,
+                ...(result || {})
+        };
+    }
+};
+method._id = actionId;
+return [key, method];
+}));
+};
+
+export const getActionIds = (actionCreators) => {
+    return _.mapValues(actionCreators, (value, key) => {
+        return value._id;
+    });
+};
+
+export const createStore = (initialState, handlers) => {
+    return (state = initialState, action) => {
+        return handlers[action.type] ?
+            handlers[action.type](state, action) :
+            state;
+    };
+};
