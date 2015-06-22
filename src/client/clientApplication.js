6bca2d1fc6171d083d3519be14bbabf7a7e366e8
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-06-22 07:42:59 -0400

    #27 Client-rendering should include middlewares in redux

--- src/client/clientApplication.js
+++ src/client/clientApplication.js
@@ -4,11 +4,17 @@ import React from 'react';
 import Root from './../containers/Root';
 import Router from 'react-router';
 import Routes from './../Routes';
-import { createRedux } from 'redux';
-import { Provider } from 'redux/react';
+import {createDispatcher, createRedux, composeStores} from 'redux'
+import {Provider} from 'redux/react';
 import * as stores from '../stores/index';
+import * as middlewares from './../middleware';
 
-const redux = createRedux(stores, window.__APP_STATE__);
+const {loggerMiddleware, thunkMiddleware, promiseMiddleware} = middlewares;
+const dispatcher = createDispatcher(
+    composeStores(stores),
+        getState => [promiseMiddleware, thunkMiddleware(getState), loggerMiddleware]
+);
+const redux = createRedux(dispatcher);
 
 Router.run(Routes, Router.HistoryLocation, (Handler, state) => {
     React.render(<Root redux={redux} handler={Handler} {...state}/>, document.body);
