3caf61c3e2315dc3f35c5ee6c5fb16376e7aa2b3
Andrew Smith <andrew@WeAreSoftwareCraftsmen.com>
2015-07-09 17:12:44 -0400

    Support promises within thunks

--- src/middleware/thunk.js
+++ src/middleware/thunk.js
@@ -2,11 +2,21 @@
 
 export default function thunk(getState) {
     return (next) => {
-        const recurse = (action) =>
-            typeof action === 'function' ?
-                action(recurse, getState) :
-                next(action);
-
+        const recurse = (action) => {
+            if (typeof action === 'function') {
+                var result = action(recurse, getState);
+                if (result.promise instanceof Promise) {
+                    const [REQUEST, SUCCESS, FAILURE] = result.types;
+                    var {...rest} = result;
+                    next({...rest, type: REQUEST});
+                    return result.promise.then(
+                        (result) => next({...rest, result, type: SUCCESS}),
+                        (error) => next({...rest, error, type: FAILURE})
+                    );
+                }
+            }
+            next(action);
+        };
         return recurse;
     };
 }
